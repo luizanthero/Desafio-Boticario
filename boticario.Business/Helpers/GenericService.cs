14ebef1412e3c0610a8ac06f12bb8aa11af22727
Luiz Anthero Gama <luizanthero@icloud.com>
2020-08-11 20:40:50 -0300

    adicionando servico generico

---
+++ boticario.Business/Helpers/GenericService.cs
@@ -0,0 +1,51 @@
+﻿using boticario.Context;
+using boticario.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace boticario.Helpers
+{
+    public class GenericService
+    {
+        private readonly AppDbContext context;
+
+        public GenericService(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<T> GetEntityAntiga<T>(int id) where T : class
+        {
+            try
+            {
+                if (typeof(T).Equals(typeof(Compra)))
+                    return await context.Compras.AsNoTracking().FirstOrDefaultAsync(item => item.Id.Equals(id)) as T;
+
+                if (typeof(T).Equals(typeof(Historico)))
+                    return await context.Historicos.AsNoTracking().FirstOrDefaultAsync(item => item.Id.Equals(id)) as T;
+
+                if (typeof(T).Equals(typeof(ParametroSistema)))
+                    return await context.ParametrosSistema.AsNoTracking().FirstOrDefaultAsync(item => item.Id.Equals(id)) as T;
+
+                if (typeof(T).Equals(typeof(RegraCashback)))
+                    return await context.RegrasCashback.AsNoTracking().FirstOrDefaultAsync(item => item.Id.Equals(id)) as T;
+
+                if (typeof(T).Equals(typeof(Revendedor)))
+                    return await context.Revendedores.AsNoTracking().FirstOrDefaultAsync(item => item.Id.Equals(id)) as T;
+
+                if (typeof(T).Equals(typeof(StatusCompra)))
+                    return await context.StatusCompra.AsNoTracking().FirstOrDefaultAsync(item => item.Id.Equals(id)) as T;
+
+                if (typeof(T).Equals(typeof(TipoHistorico)))
+                    return await context.TiposHistorico.AsNoTracking().FirstOrDefaultAsync(item => item.Id.Equals(id)) as T;
+
+                return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
