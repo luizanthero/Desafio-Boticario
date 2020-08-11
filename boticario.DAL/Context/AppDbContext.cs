473c1fe3a474c3e7e15e896f1f65427f022dc216
Luiz Anthero Gama <luizanthero@icloud.com>
2020-08-11 20:03:38 -0300

    Adicionando DAL no projeto

---
+++ boticario.DAL/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using boticario.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace boticario.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Compra> Compras { get; set; }
+        public DbSet<Historico> Historicos { get; set; }
+        public DbSet<ParametroSistema> ParametrosSistema { get; set; }
+        public DbSet<RegraCashback> RegrasCashback { get; set; }
+        public DbSet<Revendedor> Revendedores { get; set; }
+        public DbSet<StatusCompra> StatusCompra { get; set; }
+        public DbSet<TipoHistorico> TiposHistorico { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+}
