fefffb79ae4a47cceb84e893b6e898acc913a867
Luiz Anthero Gama <luizanthero@icloud.com>
2020-08-12 15:27:12 -0300

    ajustando ViewModel de Autenticacao

--- boticario.Domain/ViewModels/AuthenticationViewModel.cs
+++ boticario.Domain/ViewModels/AuthenticationViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace boticario.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace boticario.ViewModels
 {
     public class AuthenticationViewModel
     {
+        [Required]
         public string Email { get; set; }
 
+        [Required]
         public string Senha { get; set; }
     }
 }
