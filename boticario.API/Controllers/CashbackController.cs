22ed9c703fde75dc9d1ae1a5ad5d738362b3126b
Luiz Anthero Gama <luizanthero@icloud.com>
2020-08-15 18:09:31 -0300

    adicionando log no Cashback

--- boticario.API/Controllers/CashbackController.cs
+++ boticario.API/Controllers/CashbackController.cs
@@ -2,10 +2,12 @@
 using System.Threading.Tasks;
 using boticario.Helpers.Enums;
 using boticario.Models;
+using boticario.Options;
 using boticario.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace boticario.API.Controllers
 {
@@ -15,10 +17,14 @@ namespace boticario.API.Controllers
     public class CashbackController : ControllerBase
     {
         private readonly CashbackService service;
+        private readonly ILogger<CashbackController> logger;
 
-        public CashbackController(CashbackService service)
+        private readonly string controllerName = nameof(CashbackController);
+
+        public CashbackController(CashbackService service, ILogger<CashbackController> logger)
         {
             this.service = service;
+            this.logger = logger;
         }
 
         /// <summary>
@@ -35,17 +41,34 @@ namespace boticario.API.Controllers
         [HttpGet("{cpf}")]
         public async Task<ActionResult<Cashback>> GetCashbackPoints(string cpf)
         {
+            const string endpointName = nameof(GetCashbackPoints);
+            string header = $"GET | {UserTokenOptions.GetClaimTypesNameValue(User.Identity)} | {controllerName}: {endpointName}";
+
             try
             {
-                Cashback cashback = await service.GetCashbackPoints(cpf);
+                logger.LogInformation((int)LogEventEnum.Events.GetItem,
+                    $"{header} - {MessageLog.Start.Value}");
+
+                Cashback cashback = await service.GetCashbackPoints(cpf, UserTokenOptions.GetClaimTypesNameValue(User.Identity));
 
                 if (cashback is null)
+                {
+                    logger.LogWarning((int)LogEventEnum.Events.GetItem,
+                        $"{header} - {MessageError.NotFoundSingle.Value}");
+
                     return NotFound(new { message = MessageError.NotFoundSingle.Value });
+                }
+
+                logger.LogInformation((int)LogEventEnum.Events.GetItem,
+                    $"{header} - {MessageLog.Stop.Value}");
 
                 return Ok(cashback.Body);
             }
             catch (Exception ex)
             {
+                logger.LogError((int)LogEventEnum.Events.GetItem, ex,
+                    $"{header} - {MessageLog.Error.Value} | Exception: {ex.Message}");
+
                 return StatusCode(StatusCodes.Status500InternalServerError,
                     new { message = MessageError.InternalError.Value, error = ex.Message });
             }
--- boticario.Business/Services/CashbackService.cs
+++ boticario.Business/Services/CashbackService.cs
@@ -1,5 +1,8 @@
 ﻿using boticario.ExternalAPIs.boticario;
+using boticario.Helpers.Enums;
 using boticario.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System;
 using System.Threading.Tasks;
 
@@ -7,11 +10,29 @@ namespace boticario.Services
 {
     public class CashbackService
     {
-        public async Task<Cashback> GetCashbackPoints(string cpf)
+        private readonly ILogger<CashbackService> logger;
+
+        private readonly string serviceName = nameof(CashbackService);
+
+        public CashbackService(ILogger<CashbackService> logger)
         {
+            this.logger = logger;
+        }
+
+        public async Task<Cashback> GetCashbackPoints(string cpf, string usuario)
+        {
+            const string methodName = nameof(GetCashbackPoints);
+            string header = $"METHOD | {usuario} | {serviceName}: {methodName}";
+
             try
             {
+                logger.LogInformation((int)LogEventEnum.Events.GetItem,
+                    $"{header} - {MessageLog.Getting.Value}");
+
                 Cashback result = await BoticarioConnection.Connect<Cashback>($"?cpf={cpf}");
+
+                logger.LogInformation((int)LogEventEnum.Events.GetItem,
+                    $"{header} - {MessageLog.Getted.Value} - Credit: {result.Body.Credit}");
 
                 return result;
             }
