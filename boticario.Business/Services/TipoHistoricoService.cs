76b9e0b5f472a8b5b8beda477953950b5ec59bd9
Luiz Anthero Gama <luizanthero@icloud.com>
2020-08-16 12:08:29 -0300

    adicionando log na controller Tipo Historico

--- boticario.Business/Services/TipoHistoricoService.cs
+++ boticario.Business/Services/TipoHistoricoService.cs
@@ -3,6 +3,7 @@ using boticario.Helpers;
 using boticario.Helpers.Enums;
 using boticario.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -18,20 +19,35 @@ namespace boticario.Services
         private readonly HistoricoService historicoService;
         private readonly HelperService helperService;
 
-        public TipoHistoricoService(AppDbContext context, HistoricoService historicoService, HelperService helperService)
+        private readonly ILogger<TipoHistoricoService> logger;
+
+        private readonly string serviceName = nameof(TipoHistoricoService);
+
+        public TipoHistoricoService(AppDbContext context, HistoricoService historicoService, HelperService helperService,
+            ILogger<TipoHistoricoService> logger)
         {
             this.context = context;
             this.historicoService = historicoService;
             this.helperService = helperService;
+            this.logger = logger;
         }
 
         public async Task<TipoHistorico> Create(TipoHistorico entity, string usuario)
         {
+            const string methodName = nameof(Create);
+            string header = $"METHOD | {usuario} | {serviceName}: {methodName}";
+
             try
             {
+                logger.LogInformation((int)LogEventEnum.Events.InsertItem,
+                    $"{header} - {MessageLog.Saving.Value}");
+
                 context.TiposHistorico.Add(entity);
 
                 await context.SaveChangesAsync();
+
+                logger.LogInformation((int)LogEventEnum.Events.InsertItem,
+                    $"{header} - {MessageLog.Saved.Value} - ID: {entity.Id}");
 
                 string json = JsonConvert.SerializeObject(entity);
 
@@ -55,10 +71,21 @@ namespace boticario.Services
 
         public async Task<bool> DeleteById(int id, string usuario)
         {
+            const string methodName = nameof(DeleteById);
+            string header = $"METHOD | {usuario} | {serviceName}: {methodName}";
+
+            logger.LogInformation((int)LogEventEnum.Events.DeleteItem,
+                $"{header} - {MessageLog.Deleting.Value}");
+
             TipoHistorico entity = await context.TiposHistorico.FindAsync(id);
 
             if (entity is null)
+            {
+                logger.LogWarning((int)LogEventEnum.Events.DeleteItemNotFound,
+                    $"{header} - {MessageLog.DeleteNotFound.Value} - ID: {id}");
+
                 return false;
+            }
 
             string json = JsonConvert.SerializeObject(entity);
 
@@ -69,6 +96,9 @@ namespace boticario.Services
                 context.TiposHistorico.Update(entity);
 
                 await context.SaveChangesAsync();
+
+                logger.LogInformation((int)LogEventEnum.Events.DeleteItem,
+                    $"{header} - {MessageLog.Deleted.Value} - ID: {entity.Id}");
 
                 await historicoService.Create(new Historico
                 {
@@ -88,30 +118,85 @@ namespace boticario.Services
             }
         }
 
-        public async Task<IEnumerable<TipoHistorico>> GetAll()
-            => await context.TiposHistorico.Where(item => (bool)item.Ativo).ToListAsync();
+        public async Task<IEnumerable<TipoHistorico>> GetAll(string usuario)
+        {
+            const string methodName = nameof(GetAll);
+            string header = $"METHOD | {usuario} | {serviceName}: {methodName}";
 
-        public async Task<TipoHistorico> GetById(int id)
-            => await context.TiposHistorico.FirstOrDefaultAsync(item => item.Id.Equals(id));
+            try
+            {
+                logger.LogInformation((int)LogEventEnum.Events.GetItem,
+                    $"{header} - {MessageLog.GettingList.Value}");
+
+                List<TipoHistorico> result = await context.TiposHistorico.Where(item => (bool)item.Ativo).ToListAsync();
+
+                logger.LogInformation((int)LogEventEnum.Events.GetItem,
+                    $"{header} - {MessageLog.Getted.Value} - Quantidade: {result.Count()}");
+
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<TipoHistorico> GetById(int id, string usuario)
+        {
+            const string methodName = nameof(GetById);
+            string header = $"METHOD | {usuario} | {serviceName}: {methodName}";
+
+            try
+            {
+                logger.LogInformation((int)LogEventEnum.Events.GetItem,
+                    $"{header} - {MessageLog.Getting.Value}");
+
+                TipoHistorico result = await context.TiposHistorico.FirstOrDefaultAsync(item => item.Id.Equals(id));
+
+                logger.LogInformation((int)LogEventEnum.Events.GetItem,
+                    $"{header} - {MessageLog.Getted.Value} - ID: {id}");
+
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
 
         public async Task<bool> IsExist(int id)
             => await context.TiposHistorico.AnyAsync(item => item.Id.Equals(id));
 
         public async Task<bool> Update(TipoHistorico entity, string usuario)
         {
-            TipoHistorico tipo = await helperService.GetEntityAntiga<TipoHistorico>(entity.Id);
-            string oldJson = JsonConvert.SerializeObject(tipo);
+            const string methodName = nameof(Update);
+            string header = $"METHOD | {usuario} | {serviceName}: {methodName}";
 
-            entity.DataCriacao = tipo.DataCriacao;
+            logger.LogInformation((int)LogEventEnum.Events.GetItem,
+                $"{header} - {MessageLog.Getting.Value} - {MessageLog.GettingOldEntity.Value} - ID: {entity.Id} ");
+
+            TipoHistorico oldEntity = await helperService.GetEntityAntiga<TipoHistorico>(entity.Id);
+            string oldJson = JsonConvert.SerializeObject(oldEntity);
+
+            logger.LogInformation((int)LogEventEnum.Events.GetItem,
+                $"{header} - {MessageLog.Getted.Value} - ID: {entity.Id} ");
+
+            entity.DataCriacao = oldEntity.DataCriacao;
             entity.DataAlteracao = DateTime.Now;
 
             string newJson = JsonConvert.SerializeObject(entity);
+
+            logger.LogInformation((int)LogEventEnum.Events.UpdateItem,
+                $"{header} - {MessageLog.Updating.Value} - ID: {entity.Id}");
 
             context.Entry(entity).State = EntityState.Modified;
 
             try
             {
                 await context.SaveChangesAsync();
+
+                logger.LogInformation((int)LogEventEnum.Events.UpdateItem,
+                    $"{header} - {MessageLog.Updated.Value}");
 
                 await historicoService.Create(new Historico
                 {
